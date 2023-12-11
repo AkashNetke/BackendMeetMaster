1e259ea40d47785e1d7be76a34b4c88701c26ba9
AkashNetke <akashbnetke@gmail.com>
2023-12-11 11:56:22 +0530

    Initial Commit

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.User;
+import com.service.UserServiceInterface;
+
+@RestController
+public class UserController {
+	
+	
+	@Autowired
+	UserServiceInterface userServ;
+	
+	@PostMapping("/register")
+	@CrossOrigin(origins = "http://localhost:4200/")
+
+	public User RegisterUser(@RequestBody User user) throws Exception {
+		
+		String Temp=user.getEmailid();
+		if(Temp!=null && !"" .equals(Temp))
+		{
+			User obj=userServ.FindByEmailid(Temp);
+			
+			if(obj!=null)
+			{
+				throw new Exception("User with " +Temp+ "already exists ");
+			}
+		}
+		return userServ.RegisterUser(user);
+		
+		
+	
+	
+	}
+	
+	@PostMapping("/login")
+	@CrossOrigin(origins = "http://localhost:4200/")
+	public User FindByEmailidAndPassword(@RequestBody User user) throws Exception {
+	
+		String TempEmail=user.getEmailid();
+		String TempPass=user.getPassword();
+		
+		User userobj=null;
+		if(TempEmail!=null && TempPass!=null)
+		{
+			userobj=userServ.FindByEmailidAndPassword(TempEmail, TempPass);
+			
+		}
+		if(userobj==null)
+		{
+			throw new Exception("Bad Credentials");
+		}
+		return userobj;
+	}
+	
+
+	
+}
